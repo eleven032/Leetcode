a62d700ea602495b0b4333ab56f41482bf271319
eleven032 <eleven032@gmail.com>
2018-06-08 11:33:24 +0800

    new way

--- 6.Remove dup elements/remove dup.java
+++ 6.Remove dup elements/remove dup.java
@@ -1,26 +1,41 @@
 public class Solution {
     public int removeDuplicates(int[] A) {
 
-        //边际测试，传进来个null，或者没元素玩个屁啊
-        if (A == null || A.length == 0) {
-            return 0;
+        //边际测试
+        if (A.length < 2) {
+            return A.length;
         }
         
+        
+        //设定两个指针，一前一后
+        int j = 0;
+        int i = 1;
+     
 
-        //声明起始的返回值
-        int size = 0;
-
-        //用单指针循环遍历
-        for (int i = 0; i < A.length; i++) {
-
-            //当前array元素与A[size]不同时，进入如下循环
-            if (A[i] != A[size]) {
+        //后指针只要小于总长，不断循环
+        while (i < A.length) {
 
 
-                //
-                A[++size] = A[i];
+            //如果出现相邻重复元素
+            if (A[i] == A[j]) {
+                //后指针后移一位，继续走循环
+                i++;
+
+            //不相等
+            } else {
+
+                //前指针后移一位
+                j++;
+
+                //将j所在位置覆盖为新元素
+                A[j] = A[i];
+
+                //后指针后移一位
+                i++;
             }
         }
-        return size + 1;
+     
+        //返回值
+        return j + 1;
     }
 }
